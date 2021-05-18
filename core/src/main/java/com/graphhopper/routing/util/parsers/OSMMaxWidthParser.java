fdc4c5429262c4af793f5e7c5b6a1d0bf1b5a657
Henri <henri97@outlook.com>
2021-05-18 20:09:56 +0200

    Use min_maxwidth from new PBF format

--- core/src/main/java/com/graphhopper/routing/util/parsers/OSMMaxWidthParser.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/OSMMaxWidthParser.java
@@ -47,7 +47,7 @@ public class OSMMaxWidthParser implements TagParser {
 
     @Override
     public IntsRef handleWayTags(IntsRef edgeFlags, ReaderWay way, boolean ferry, IntsRef relationFlags) {
-        List<String> widthTags = Arrays.asList("cycleway:right:width", "cycleway:left:width", "maxwidth", "maxwidth:physical", "width", "cycleway:both:width", "cycleway:width");
+        List<String> widthTags = Arrays.asList("min_maxwidth", "maxwidth", "maxwidth:physical", "width", "cycleway:both:width", "cycleway:width", "cycleway:right:width", "cycleway:left:width");
         OSMValueExtractor.extractMeter(edgeFlags, way, widthEncoder, widthTags);
         return edgeFlags;
     }
